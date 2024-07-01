e4e135af6bdf1759ad620e25c2091278c6cc4848
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-07-02 03:24:22 +0530

    h

---
+++ build/precache-manifest.8238bcc764e2ccf625e60d76c26fcd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e972ff18029641aa42c3dd0b32cced3",
+    "url": "/Sachiinkk/My_Portfolio/index.html"
+  },
+  {
+    "revision": "57c6085c103fa469e357",
+    "url": "/Sachiinkk/My_Portfolio/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "d154da422b8339b2bbfe",
+    "url": "/Sachiinkk/My_Portfolio/static/js/2.b593b99c.chunk.js"
+  },
+  {
+    "revision": "57c6085c103fa469e357",
+    "url": "/Sachiinkk/My_Portfolio/static/js/main.d211f8ab.chunk.js"
+  },
+  {
+    "revision": "18077b6daf948e7dcb8e",
+    "url": "/Sachiinkk/My_Portfolio/static/js/runtime~main.faed3df3.js"
+  },
+  {
+    "revision": "d8fe052d1d3528fb2f9901a68959cd9f",
+    "url": "/Sachiinkk/My_Portfolio/static/media/HeroImage.d8fe052d.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa7daed739816c012e8ab5772c5d1673.js"
+  "/Sachiinkk/My_Portfolio/precache-manifest.8238bcc764e2ccf625e60d76c26fcd63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sachiinkk/My_Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
