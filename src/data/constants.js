9768353f307d702095b2b22743dfeab6a1328aae
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-07-20 18:40:11 +0530

    last commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -159,7 +159,7 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
     role: " Fullstack Engineering Intern",
     company: "Design Innovation Centre , Panjab University ",
     date: "june 2023 - July 2023",
